4af605a5797e3120f92d335a80f3594dc86d417a
System Administrator <root@Davids-MacBook-Pro.local>
2017-09-23 17:04:13 -0700

    Allow playback and record

--- src/ios/CDVSound.m
+++ src/ios/CDVSound.m
@@ -364,8 +364,8 @@
                     bPlayAudioWhenScreenIsLocked = [playAudioWhenScreenIsLocked boolValue];
                 }
 
-                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayback : AVAudioSessionCategorySoloAmbient;
-                [self.avSession setCategory:sessionCategory error:&err];
+                NSString* sessionCategory = bPlayAudioWhenScreenIsLocked ? AVAudioSessionCategoryPlayAndRecord : AVAudioSessionCategorySoloAmbient;
+                [self.avSession setCategory:sessionCategory withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&err];
                 if (![self.avSession setActive:YES error:&err]) {
                     // other audio with higher priority that does not allow mixing could cause this to fail
                     NSLog(@"Unable to play audio: %@", [err localizedFailureReason]);
